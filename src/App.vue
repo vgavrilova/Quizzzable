3881d487afdbc522b4b09de2a4500a393d91795c
Valeria Gavrilova <gavrivalery@gmail.com>
2021-02-23 18:06:41 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox :question="questions[index]" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    QuestionBox,
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+    };
+  },
+  mounted() {
+    fetch(
+      "https://opentdb.com/api.php?amount=15&type=multiple&encode=url3986",
+      {
+        method: "get",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.questions = data.results;
+      });
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled><b>Quizzzable</b></b-nav-item>
+      <b-nav-item disabled>Counter: 2/4</b-nav-item>
+    </b-nav>
+  </div>
+</template>
